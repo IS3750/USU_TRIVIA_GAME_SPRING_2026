36a40734d8ec3557a709694d5e4353224eb8c5b0
PunkPrincessMay <punk.princess.may@gmail.com>
2026-03-05 19:54:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// Write your functions here:
+//Name: Katie, Description: This JS file writes a triva question function to put in an HTML file with others
+
 // 1) Each function should accept points as an input
-// 2) Each function should display a prompt quizzing the user on the correct answer to a piece of USU trivia (You choose the trivia question)
-// 3) If they answer correctly, return the input points + however many points you feel they deserve for being correct.
-// 4) Otherwise, return the original score
 // 5) Name your function according to the guidelines on canvas (e.g. round1, round2, etc.)
-// If you need more guidence, feel free to check index.html to see the main flow of the program.
+export const round3 = (points) => {
+
+    // 2) Each function should display a prompt quizzing the user on the correct answer to a piece of USU trivia (You choose the trivia question)
+    let trivQuest = prompt("What was the name of the USU club that started the tradition of True Aggie Night?").toLowerCase();
+
+    // 3) If they answer correctly, return the input points + however many points you feel they deserve for being correct.
+    // 4) Otherwise, return the original score
+    if (trivQuest === "beno club"){
+        alert(`Correct! Your original score was ${points}, and you earned 100 points. Your score is now: ${Number((points+100))}`);
+    }else if (trivQuest !== "beno club"){
+        alert(`Unfortunately, that is incorrect. The answer was "Beno Club". Your score is: ${Number(points)}`);
+    }else{
+        alert("Okay, something is definitely wrong with the code.");
+    };
+};
